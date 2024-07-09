b05f0a95934ac59d63bb3ad79056ab9c6937db71
Je4nComJ <jeanvictor.fa@gmail.com>
2024-07-09 18:34:42 -0300

    criada tabela de participantes e entidade e repository de participante

---
+++ backend/src/main/java/com/gomesdevelopers/planner/participant/ParticipantRepository.java
@@ -0,0 +1,8 @@
+package com.gomesdevelopers.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+}
